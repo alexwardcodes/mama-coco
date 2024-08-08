a9e34eeb4560d090b18557b30f92f521414f60cf
Sasha Ward <sashaward@00563-SashaW.local>
2024-08-08 23:31:03 +0100

    Created holding page for Mama Coco business site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Image, Flex, Text } from '@chakra-ui/react';
+import logo from "./images/mama-coco-logo.jpg"
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Flex width="100vw" height="100vh" alignItems="center" justifyContent="center" direction="column">
+      <Image src={logo} width="380px" alignSelf="center" />
+    <Text fontSize="30px" fontWeight="bold" color="#40311D">Coming soon...</Text>
+    </Flex>
+  )
+};
 
 export default App;
